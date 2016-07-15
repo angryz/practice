f9b6ca5c0eb7bd481be2956e0932ac3ca96a225f
angryz <angryzorro@gmail.com>
2016-07-15 17:30:49 +0800

    Add spring boot Example

---
+++ spring_boot/src/main/java/info/noconfuse/practice/springboot/Example.java
@@ -0,0 +1,25 @@
+package info.noconfuse.practice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#getting-started-first-application-code
+ * <p>
+ * Created by zzp on 7/15/16.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
