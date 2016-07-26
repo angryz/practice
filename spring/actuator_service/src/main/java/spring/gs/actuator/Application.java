2c2fe4a177b91541667557bf753c96a940f25bf4
angryz <angryzorro@gmail.com>
2016-07-26 18:20:47 +0800

    Run spring boot actuator

---
+++ spring/actuator_service/src/main/java/spring/gs/actuator/Application.java
@@ -0,0 +1,15 @@
+package spring.gs.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zzp on 7/26/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
