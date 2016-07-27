383e71ed33d2cc44c08ad8115da405af80c55831
angryz <angryzorro@gmail.com>
2016-07-27 16:37:34 +0800

    Add security features

---
+++ spring/securing_web/src/main/java/spring/gs/securing/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package spring.gs.securing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * This class is annotated with @EnableWebSecurity to enable Spring Security’s web security support
+ * and provide the Spring MVC integration.
+ * It also extends WebSecurityConfigurerAdapter and overrides a couple of its methods to set some specifics
+ * of the web security configuration.
+ * <p>
+ * Created by zzp on 7/27/16.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+}
