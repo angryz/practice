dba16c5cdbe405903673e399e89bf382bd84343e
angryz <angryzorro@gmail.com>
2016-07-27 15:55:50 +0800

    Add thymeleaf templates then run the application

---
+++ spring/securing_web/src/main/java/spring/gs/securing/Application.java
@@ -0,0 +1,15 @@
+package spring.gs.securing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zzp on 7/27/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring/securing_web/src/main/java/spring/gs/securing/MvcConfig.java
@@ -0,0 +1,23 @@
+package spring.gs.securing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * The web application is based on Spring MVC.
+ * Thus you need to configure Spring MVC and set up view controllers to expose these templates.
+ * <p>
+ * Created by zzp on 7/27/16.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
