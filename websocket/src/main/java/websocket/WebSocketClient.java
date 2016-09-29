06f2f59f802d5aac1e3530d6aad1a77d0b9ad16e
angryz <angryzorro@gmail.com>
2016-09-29 15:01:29 +0800

    Practice websocket client with netty.

---
+++ websocket/src/main/java/websocket/WebSocketClient.java
@@ -0,0 +1,111 @@
+package websocket;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
+import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author Zheng Zhipeng
+ */
+public class WebSocketClient {
+
+    static final String WS_URL = "ws://127.0.0.1:8000/websocket";
+
+    public static void main(String[] args) throws URISyntaxException, InterruptedException, IOException {
+        URI uri = new URI(WS_URL);
+        String scheme = uri.getScheme() == null ? "ws" : uri.getScheme();
+        final String host = uri.getHost() == null ? "127.0.0.1" : uri.getHost();
+        final int port;
+        if (uri.getPort() == -1) {
+            if ("ws".equalsIgnoreCase(scheme)) {
+                port = 8000;
+            } else if ("wss".equalsIgnoreCase(scheme)) {
+                port = 8443;
+            } else {
+                port = -1;
+            }
+        } else {
+            port = uri.getPort();
+        }
+
+        if (!"ws".equalsIgnoreCase(scheme) && !"wss".equalsIgnoreCase(scheme)) {
+            System.err.println("Only WS(S) is supported.");
+            return;
+        }
+        System.out.println("Scheme: " + scheme + ", Host: " + host + ", Port: " + port);
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            final WebSocketClientProtocolHandler handler = new WebSocketClientProtocolHandler(
+                    WebSocketClientHandshakerFactory.newHandshaker(
+                            uri,
+                            WebSocketVersion.V13,
+                            null,
+                            false,
+                            new DefaultHttpHeaders()
+                    )
+            );
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(
+                                    new HttpClientCodec(),
+                                    new HttpObjectAggregator(8192),
+                                    WebSocketClientCompressionHandler.INSTANCE,
+                                    handler
+                            );
+                        }
+                    });
+            Channel ch = b.connect(host, port).sync().channel();
+            handler.handshaker().handshake(ch).sync();
+
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String msg = console.readLine();
+                if (msg == null) {
+                    break;
+                } else if ("bye".equals(msg.toLowerCase())) {
+                    ch.writeAndFlush(new CloseWebSocketFrame());
+                    ch.closeFuture().sync();
+                    break;
+                } else if ("ping".equals(msg.toLowerCase())) {
+                    WebSocketFrame frame = new PingWebSocketFrame(Unpooled.wrappedBuffer(new
+                            byte[]{8, 1, 8, 1}));
+                    ch.writeAndFlush(frame);
+                } else {
+                    WebSocketFrame frame = new TextWebSocketFrame(msg);
+                    ch.writeAndFlush(frame);
+                }
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
