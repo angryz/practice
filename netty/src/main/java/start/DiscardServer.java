441d5ad095dfe6f17ccdd90af1c8d4c0931de42e
angryz <angryzorro@gmail.com>
2016-09-30 16:38:03 +0800

    Put DiscardServerHandler into DiscardServer.

--- netty/src/main/java/start/DiscardServer.java
+++ netty/src/main/java/start/DiscardServer.java
@@ -1,13 +1,17 @@
 package start;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -61,5 +65,48 @@ public class DiscardServer {
             port = 8080;
         }
         new DiscardServer(port).run();
+    }
+
+    public static class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+        private static final Logger logger = LoggerFactory.getLogger(DiscardServerHandler.class);
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            logger.debug("Channel [{}] received [{}]", ctx.channel().toString(), msg.toString());
+            ByteBuf in = (ByteBuf) msg;
+            try {
+                while (in.isReadable()) {
+                    System.out.print((char) in.readByte());
+                    System.out.flush();
+                }
+            } finally {
+                ReferenceCountUtil.release(msg);
+            }
+
+            // discard the received data silently.
+            //in.release();
+
+        /*
+         * ByteBuf is a reference-counted object which has to be released explicitly via
+         * the release() method. Please keep in mind that it is the handler's responsibility to
+         * release any reference-counted object passed to the handler.
+         */
+            // usually, this method is implemented like the following:
+        /*
+        try {
+            // do sth. with the message
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+        */
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            // close the connection when an exception is raised.
+            cause.printStackTrace();
+            ctx.close();
+        }
     }
 }
