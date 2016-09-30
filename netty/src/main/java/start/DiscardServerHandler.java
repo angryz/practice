6a4e1c1e936cf99ec51c367914e453dc86dda649
angryz <angryzorro@gmail.com>
2016-09-30 16:15:02 +0800

    Print incoming message before discard it.

--- netty/src/main/java/start/DiscardServerHandler.java
+++ netty/src/main/java/start/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package start;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +19,19 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         logger.debug("Channel [{}] received [{}]", ctx.channel().toString(), msg.toString());
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+
         // discard the received data silently.
-        ((ByteBuf) msg).release();
+        //in.release();
+
         /*
          * ByteBuf is a reference-counted object which has to be released explicitly via
          * the release() method. Please keep in mind that it is the handler's responsibility to
